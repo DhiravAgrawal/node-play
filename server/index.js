8ada63734e809fae86bc623f9b3fa79705b6d937
[DHirav Agrawal] <[dhirav128@gmail.com]>
2024-05-21 19:54:54 +0530

    node:server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const http = require("http");
 const file = require("fs");
-file.writeFile("./logs.txt","\n",(err)=>{console.error(err)})
+const url = require("url");
 const myServer = http.createServer((req,res)=>{
+    if(req.url==="./favicon.ico") return res.end();
     const log = `${Date.now()}: New req Recieved\n`;
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
     file.appendFile("./logs.txt", log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case "/" : res.end("HomePage");
             break
-            case "/about" : res.end("Yo soy Dhirav Agrawal");
+            case "/about" : 
+            const username = myUrl.query.Dhirav;
+            res.end(`Hi, ${username}`);f
             break
             default :
             res.end("404 Not Found");
